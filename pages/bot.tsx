a13c98cce6b2d658a18a14363182bb945a99f310
Olle-88 <ollelundkvist1988@gmail.com>
2025-04-23 14:47:52 +0200

    Disable TS checking on chatbot page

--- pages/bot.tsx
+++ pages/bot.tsx
@@ -1,20 +1,31 @@
-import Head from "next/head";
+// pages/bot.tsx
+// @ts-nocheck
+import Head from "next/head"
 
 export default function BotPage() {
   return (
     <>
       <Head>
+        {/* Ladda in Dialogflow Messenger-scriptet */}
         <script
           src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"
           async
         />
       </Head>
+
+      {/* Nu hoppar TS-typen för <df-messenger> över */}
       <df-messenger
         intent="WELCOME"
         chat-title="Konstboten"
         agent-id="c28d8b31-05a0-4078-b3b4-853f943e2c86"
-        language-code="en"
+        language-code="sv"
       />
     </>
-  );
+  )
+}
+
+// (Din pageConfig om du behöver)
+BotPage.pageConfig = {
+  bgClass: "bg-[url('/images/reflektion-bg.jpg')] bg-cover bg-center",
+  textClass: "text-white",
 }
